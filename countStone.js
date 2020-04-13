14bb7c2b344f03252375fe5bcd86551ac6d00bb3
nhonnhon <39259094+nhonnhon@users.noreply.github.com>
2020-04-13 10:14:02 +0700

    Create countStone.js

---
+++ countStone.js
@@ -0,0 +1,18 @@
+function countStone(j, s) {
+  const arrS = [...s]
+  console.log(arrS)
+  const sLen = s.length
+  let count = 0;
+  
+  for (let i=0; i<sLen; i++) {
+    if (j.includes(arrS[i])) {
+      count++;
+    }
+  }
+  
+  return count;
+}
+
+console.log(countStone("aA","aAAbbbb"))
+console.log(countStone("z","ZZ"))
+console.log(countStone("Z","zz"))
